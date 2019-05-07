f726d6dc82625b3b22b0e1172d5640a8ec6630ca
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-07 02:16:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,5 +46,12 @@ function createIssue() {
 }
 
 function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
+  const url = 'https://api.github.com/repos/mattetress/js-ajax-fetch-lab/issues'
+
+  fetch(url, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json())
+    .then(json => console.log(json));
 }
